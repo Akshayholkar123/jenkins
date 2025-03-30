242a5e1e4dc369120daa6653c7c6490cc9e8d7c3
Akshayholkar123 <aholkar01@gmail.com>
2025-03-30 13:57:02 +0530

    25firstCommit

---
+++ src/test/java/Org/MavenCmdTest.java
@@ -0,0 +1,13 @@
+package Org;
+
+import org.testng.annotations.Test;
+
+public class MavenCmdTest {
+	
+	@Test
+	public void runCmdTest()
+	{
+		System.out.println("executed via cmd");
+	}
+
+}
---
+++ src/test/java/Org/OrgTest.java
@@ -0,0 +1,43 @@
+package Org;
+
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.Test;
+
+public class OrgTest {
+    WebDriver driver;
+
+    @Test(groups = "regression")
+    public void org() {
+        String browser = System.getProperty("browser", "chrome"); // Default to Chrome
+        System.out.println("Browser: " + browser);
+        System.out.println("Regression testing");
+        System.out.println("Holkar");
+
+        // Launch browser based on the system property
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            System.out.println("Invalid browser specified: " + browser);
+            return; // Exit if invalid browser
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("https://www.flipkart.com");
+    }
+
+    @Test(groups = "smoke")
+    public void createorg() {
+        String browser = System.getProperty("browser", "chrome");
+        System.out.println("Browser: " + browser);
+        System.out.println("Smoke testing");
+    }
+}
